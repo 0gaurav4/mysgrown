2bd55a7acc2ab957b302907f83b192bd12967fe9
Gaurav Tiwari <gauravtsm@gmail.com>
2024-01-19 17:51:25 +0530

    chatbot instance

--- scripts.js
+++ scripts.js
@@ -72,6 +72,7 @@ function hideLoader() {
     instance.updateLauncherAvatarURL('./images/robot.png');
     await instance.render(); }
   };
+  
   setTimeout(function(){
     const t=document.createElement('script');
     t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
